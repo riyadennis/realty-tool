59efa7cb432f35ea3af0db9982f038f356351a6a
Riya Dennis <riya.dennis@adarga.ai>
2022-02-02 14:33:07 +0000

    add scraping back

--- cmd/main.go
+++ cmd/main.go
@@ -4,23 +4,26 @@ import (
 	"log"
 	"os"
 
+	"github.com/riyadennis/realty-tool/ex/scrape"
+
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/riyadennis/realty-tool/ex/registry"
 )
 
 func main() {
-	//urls := scrape.GetURLS("config.yaml")
-	//if urls != nil {
-	//	records := scrape.Search(urls.Urls)
-	//	if len(records) == 0 {
-	//		panic("no records")
-	//	}
-	//	err := scrape.CheckAndUpdate(records)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//}
+	urls := scrape.GetURLS("config.yaml")
+	if urls != nil {
+		records := scrape.Search(urls.Urls)
+		if len(records) == 0 {
+			panic("no records")
+		}
+		err := scrape.CheckAndUpdate(records)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	logger := log.New(os.Stdout, "LOADER: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 	loader := &registry.Loader{}
 	err := registry.LoadFromCSV(logger, "data/input.csv", "output.rdf", loader)
